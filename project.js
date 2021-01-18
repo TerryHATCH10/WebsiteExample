bec79e45b2d25142b4588d6f9c1b8d672e9200c8
TerryHATCH10 <77639831+TerryHATCH10@users.noreply.github.com>
2021-01-18 15:02:22 -0500

    Create project.js

---
+++ project.js
@@ -0,0 +1,11 @@
+var setup = function () {
+  
+  createCanvas(400,400);
+};
+
+var draw = function() {
+    background(120, 240, 51);
+    fill(0,0,0);
+    ellipse(mouseX, mouseY, 50,50);
+    
+};
